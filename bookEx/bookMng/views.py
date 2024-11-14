e5ac6f04c0b7d2c730f510eedcad801fb1a3862f
miiaal <168029939+miiaal@users.noreply.github.com>
2024-11-13 17:31:06 -0800

    Add message

--- bookEx/bookMng/views.py
+++ bookEx/bookMng/views.py
@@ -21,6 +21,7 @@ def index(request):
 def postbook(request):
     print("Hello -Ira")
     print("Hello -tristian")
+    print("Hello :D - Mia")
     submitted = False
     if request.method == 'POST':
         form = BookForm(request.POST, request.FILES)
